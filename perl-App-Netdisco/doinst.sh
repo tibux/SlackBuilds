309491004dac810e135fae0393d3024bb2b335d7
Thibaut Notteboom <tib@tibux.org>
2013-05-07 23:25:04 +0200

    splitt init script in two scripts

--- perl-App-Netdisco/doinst.sh
+++ perl-App-Netdisco/doinst.sh
@@ -22,5 +22,6 @@ preserve_perms() {
   config $NEW
 }
 
-preserve_perms etc/rc.d/rc.netdisco.new
+preserve_perms etc/rc.d/rc.netdisco-daemon.new
+preserve_perms etc/rc.d/rc.netdisco-web.new
 config etc/netdisco.conf.new
