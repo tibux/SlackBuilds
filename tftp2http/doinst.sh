1cff1aab9794c58871339873e91852c7ada91180
tibux <tib@tibux.org>
2013-12-07 21:58:31 +0100

    Add tftp2http

---
+++ tftp2http/doinst.sh
@@ -0,0 +1,15 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/xinetd.d/tftp.new
+config etc/logrotate.d/tftp2http.new
